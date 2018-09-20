876ada92448a604751513a638482acfefbe9e856
mattbag <mattbag00@gmail.com>
2018-09-20 21:06:00 +1000

    some base styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,25 +1,26 @@
 /* global tw */
-import React from 'react'
-import Helmet from 'react-helmet'
-import { css } from 'emotion';
+import { css } from "emotion";
+import React from "react";
+import Helmet from "react-helmet";
 
-import Navbar from '../components/Navbar'
-import Socialbar from '../components/Socialbar'
+import Navbar from "../components/Navbar";
+import Socialbar from "../components/Socialbar";
 // import './all.sass'
-import './master.css'
+import "./master.css";
 
 const layoutWrap = css(tw`max-w-2xl mx-auto px-4`);
-const sticky = css(tw`sticky pin-t bg-white z-10`);
+const outernMost = css(tw`antialiased`);
+// const sticky = css(tw`sticky pin-t bg-white z-10`);
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className={outernMost}>
     <Helmet title="Home | Gatsby + Netlify CMS" />
-    <div className={sticky}>
-    <Socialbar />
+    {/* <div> */}
+    {/* <Socialbar /> */}
     <Navbar />
-    </div>
+    {/* </div> */}
     <div className={layoutWrap}>{children}</div>
   </div>
-)
+);
 
-export default TemplateWrapper
+export default TemplateWrapper;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import logo from '../img/logo.svg'
+/* global tw */
+import { css } from "emotion";
+import React from "react";
+import { Link } from "gatsby";
+
+const title = css(
+  tw`my-0 text-xl md:text-4xl leading-tight text-red-darker text-center`
+);
+const navlink = css(
+  tw`my-0 text-lg leading-tight text-red-dark no-underline mx-2`
+);
+const navflex = css(tw`flex justify-center`);
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar is-transparent" style={{ padding: ".3rem 4vw" }}>
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+        <Link to="/" className={navlink}>
+          {/* <figure className="image">
+            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+          </figure> */}
+          <h1 className={title}>Dibbs & The Gang</h1>
         </Link>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
+      <div className={navflex}>
+        <Link className={navlink} to="/about">
           Bio
         </Link>
-        <Link className="navbar-item" to="/products">
-          Date
+        <Link className={navlink} to="/products">
+          Dates
         </Link>
-        <Link className="navbar-item" to="/products">
+        <Link className={navlink} to="/products">
           Videos
         </Link>
       </div>
+      <div />
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;

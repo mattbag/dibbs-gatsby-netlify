5ad390a1fd8778ad667a9b68c6f51487d25285df
matt <matt@atlasagency.com.au>
2018-09-25 08:44:00 +1000

    plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,19 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `DIVE BELL`,
+        short_name: `DIVE BELL`,
+        start_url: `/`,
+        background_color: `#0a0a0a`,
+        theme_color: `#ffc0cb`,
+        display: `standalone`,
+        icon: `src/img/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }

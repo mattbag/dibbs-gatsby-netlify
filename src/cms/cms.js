e1d8d36219a18814801509b0cd8bc720729568cf
mattbag <mattbag00@gmail.com>
2018-09-29 14:18:12 +1000

    date previes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,11 @@
-import CMS from 'netlify-cms'
+import CMS from "netlify-cms";
+z;
+import AboutPagePreview from "./preview-templates/AboutPagePreview";
+import BlogPostPreview from "./preview-templates/BlogPostPreview";
+import DatePreview from "./preview-templates/DatePreview";
+import ProductPagePreview from "./preview-templates/ProductPagePreview";
 
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-
-CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate("about", AboutPagePreview);
+CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("blog", BlogPostPreview);
+CMS.registerPreviewTemplate("dates", DatePreview);

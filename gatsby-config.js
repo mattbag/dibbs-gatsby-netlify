516e8b1a4adad74959f1750e2c9cb4015294fbdb
matt <matt@atlasagency.com.au>
2018-10-05 09:55:33 +1000

    remark images md

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,18 @@ module.exports = {
     {
       resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [`gatsby-plugin-netlify-cms-paths`]
+        plugins: [
+          `gatsby-plugin-netlify-cms-paths`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 800,
+            },
+          },
+      ]
       }
     },
     {

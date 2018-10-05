af95ce2c27c63bc9bc7b8f4d8bdc9c327c6f9f56
matt <matt@atlasagency.com.au>
2018-10-05 14:47:22 +1000

    fix meta desc

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,11 +13,11 @@ const layoutWrap = css(tw`max-w-2xl mx-auto px-4`);
 const outernMost = css(tw`p-4`);
 // const sticky = css(tw`sticky pin-t bg-white z-10`);
 
-const TemplateWrapper = ({ children, seotitle, seodesc }) => (
-  <div className={outernMost} style={{minHeight:'90vh'}}>
+const TemplateWrapper = ({ children, seotitle, seodescription }) => (
+  <div className={outernMost} style={{ minHeight: '90vh' }}>
     <Helmet title={seotitle ? seotitle + ' | DIVE BELL' : 'DIVE BELL'} >
-    <html lang="en" />
-    <meta name="description" content={seodesc ? seodesc : 'Follow DIVE BELL'} />
+      <html lang="en" />
+      <meta name="description" content={seodescription ? seodescription : 'Follow DIVE BELL'} />
     </Helmet>
     <Socialbar />
     <Navbar />
